9ebcb9654cf32977a362f61630075bd9c659f803
unknown <08250282.sce.@rub.edu.bt>
2026-03-18 09:59:45 +0600

    learn python and learning string

---
+++ Classs Demo/stringEX.py
@@ -0,0 +1,13 @@
+print("hello")
+print("Hello")
+multiplelines='''This is a string
+that spans multiple lines.'''
+print(multiplelines)
+
+a=1
+b=2
+c=a+b
+print(type(c))#data type of c is int
+c=str(c)#Type casting
+print(type(c))#data type of c is now str 
+
