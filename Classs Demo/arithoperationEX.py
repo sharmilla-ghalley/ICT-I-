5b97956df322c08c47bbc6e96edc11a08aff574d
unknown <08250282.sce.@rub.edu.bt>
2026-03-20 15:06:55 +0600

    learn arithoperation

---
+++ Classs Demo/arithoperationEX.py
@@ -0,0 +1,14 @@
+a=10
+b=5
+addition_result=a+b#10+5=15
+print(addition_result)
+subtraction_result=a-b#10-5=5
+print(subtraction_result)
+multiplication_result=a*b#10*5=50
+print(multiplication_result)
+division_result=a/b#10/5=2.0(division always result a float)
+print(division_result)
+exponentiation_result=a**b#10**5=100000
+print(exponentiation_result)
+modulus_result=a%b#10%5=0(remainder of division)
+print(modulus_result)
