57a8ee0d58bc99ecaae1ffdccd4596c48801ad8f
unknown <08250282.sce.@rub.edu.bt>
2026-03-20 15:47:50 +0600

    learn logicaloperator

---
+++ Classs Demo/logicaloperatorEX.py
@@ -0,0 +1,12 @@
+x=True
+y=False
+result_and=x and y
+print("Logical AND:", result_and)
+
+
+result_or=x or y 
+print("Logical OR:", result_or)
+
+
+result_not=x or y
+print("Logical NOT:", result_not)
