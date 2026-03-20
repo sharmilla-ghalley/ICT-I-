82d6b62739f7afeae7a5b93971463d1a7345d05a
unknown <08250282.sce.@rub.edu.bt>
2026-03-20 16:12:58 +0600

    learn through excercise

---
+++ Classs Demo/excerciseEX.py
@@ -0,0 +1,14 @@
+print("welcome sweet user")
+name=input("Enter your name:")
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number:"))
+addition_result=num1+num2
+subtraction_result=num1+num2
+multiplication_result=num1*num2
+division_result=num1/num2
+print("Hello",name)
+print("Addition:",addition_result)
+print("Subtraction:",subtraction_result)
+print("Multiplication:",multiplication_result)
+print("Division:", division_result)
+print("sweet results")
