7b36b5ee4fffa78a185dd6811aa767470bf57aa7
unknown <08250282.sce.@rub.edu.bt>
2026-03-20 15:32:21 +0600

    learn arithoperation

---
+++ Classs Demo/areaofrectangle.py
@@ -0,0 +1,6 @@
+l=float(input("enter the length of the rectangle:"));
+b=float(input("enter the breadth: "));
+area=l*b;
+print("the area of the rectangle is:", area)
+print(type(l))
+print(type(b))
