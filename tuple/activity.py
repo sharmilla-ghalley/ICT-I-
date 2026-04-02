81701297eebb8c8aee06f2c49b71fd0a9ff25018
unknown <08250282.sce.@rub.edu.bt>
2026-04-02 16:23:05 +0600

    learn tuple

---
+++ tuple/activity.py
@@ -0,0 +1,14 @@
+my_tuple=("Hello", 123456)
+print(type(my_tuple))
+print(my_tuple)
+print(my_tuple[-1])
+a,b= my_tuple
+print(b)
+new_tup=tuple(a)
+print(new_tup)
+concatenated_tuple=my_tuple+new_tup
+print(concatenated_tuple)
+print(concatenated_tuple)
+print(concatenated_tuple)
+del my_tuple
+print(my_tuple)
