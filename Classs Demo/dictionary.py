19ca3dd20a32ca184b9d854fd7fb9c8f478e8112
unknown <08250282.sce.@rub.edu.bt>
2026-04-03 16:20:41 +0600

    learn dictionary

---
+++ Classs Demo/dictionary.py
@@ -0,0 +1,20 @@
+userDetails={'Id':1, 'userName':'Just_me'}
+print(type(userDetails))
+print(userDetails)
+location=dict(s='samtse', t='Thmphu', p='Paro')
+print(location)
+print(userDetails['userName'])
+print(location.get('t'))
+userDetails['email']='justme@example.com'
+print(userDetails)
+userDetails['userName']='Just_me_updated'
+print(userDetails)
+del location['p']
+print(location)
+
+deleted_value=userDetails.pop('email')
+print(deleted_value)
+de_key,del_value=userDetails.popitem()
+print(f'the deleted key is (del_key) and the deleted value is (del_value)')
+location.clear()
+print(location)
