c98369356ca62b5ea491367a6deff30d876f6f9b
unknown <08250282.sce.@rub.edu.bt>
2026-04-12 11:32:42 +0600

    learn lab coding

--- Classs Demo/lab0108250282.py
+++ Classs Demo/lab0108250282.py
@@ -1,20 +1,17 @@
-#Student Information Management System.
-students_list = [] #Initializing empty list to store only the names of students.
-
+# Lab task on student information management system
+students_list=[] #This list is to initialze the students name.
 #The name is a key and the other dictionary (age + grade) is a value.
 students_dict = {} #This dictionary contains complete student information inclusively age and grade.
 
 #Helping one student by taking their input.
-name = input("Enter student name: ") #You can enter the names of the student that you want to add.
-age = input("Enter student age: ") #Enter the age of the student.
-grade = input("Enter student grade: ") #Finally enter the grade of the student.
+student_name=input("Enter the name of the student:")#user the enter the name oof the required students.
+student_age=input("Enter the age of the student:")#user can enter the age of the student
+student_grade=input("Enter the grade of the student:")#Now user can enter the grade of the student.
 
-#Saving the student data.
-students_list.append(name) # Adding the names of the student to the list.
-students_dict[name] = {"age": age, "grade": grade} #Adding name as key and grade and age as value to dict{}
-
-#Indicate to the user that the data was saved.
-print("Student information has been added successfully!") #this line passes the message to user that the data is stored successfully.
+#storing the data(name,age and grade) of the student.
+students_list.append(student_name) # this is a procees to add the name of the students to the list.
+students_dict[student_name] = {"age": student_age, "grade":student_grade} #name=key and Age and Grade= value
+print("Student's information has been added successfully!")#Enabling the user to know whether the saving of data was successful or not.
 
 #Displaying all stored student information.
 print("Student Details:") #Allowing the users to view the sored student information.
@@ -26,24 +23,22 @@ for key, value in students_dict.items(): ## This loop goes through every student
     print("Grade:", value["grade"])
 
 # Find student.
-search_name = input("Enter the name of the student to be searched: ") # Allowing the user to search for a student by their name
-
-# Check whether or not student is in the records.
-if search_name in students_dict: #Check if the name entered by the user exists as a key in our dict{}.
-    print("Student Found!")# If found, it will display the student's name, age, and grade. 
-    print("Name:", search_name) # # Display the student's name that was searched for
-    print("Age:", students_dict[search_name]["age"])# Access the dict{} using the name to show the specific 'age' value
-    print("Grade:", students_dict[search_name]["grade"]) # Access the dict{} using the name to pull out the specific 'grade' value
+Search_name=input("Enter the name of the student you to search for:") #Allow user to find the student 
+#Checking the presence of the student in the records.
+if Search_name in students_dict: #check if the name entered is in the dict{} as a key
+    print("Student Found!")# If it's there then it will print the data(name, age and grade) 
+    print("Name:", Search_name)  # Display the name that was searched
+    print("Age:", students_dict[Search_name]["age"])#Access to Dict{} using the key(name) to get the value(age) and print it.
+    print("Grade:", students_dict[Search_name]["grade"]) # Access the dict{} through the key(name) to get accessed to value(grade)
 else:
-    print("Student not found!") # If not found, it will display "student not found"
+    print("Student's detail not found!") #In case it's not found it will print out not found.
 
 # Removinga student record.
-remove_name = input("Name of student to remove: ")  #Allowing the user to remove a student from the system by entering their name.
-
-# Remove student from list and dictionary, in case student exists.
-if remove_name in students_dict: # Check if the student's name exists as a key in our dictionary before trying to delete
-    students_list.remove(remove_name)# Remove the student's name from the list of names
-    del students_dict[remove_name]# Permanently delete the student's entire record (age and grade) from the dictionary
-    print("Student removed successfully!")# If found, it will remove the student's information from all data structures. 
+remove_name=input("Enter the name of the student you want to remove:") #Allow the user the remove the student details
+#removing the student's details from list and dictionary incase it's present.
+if remove_name in students_dict: # Allows user to check of the entered name exist as a key in the dict{} before the removal process.
+    students_list.remove(remove_name)# Ensure removal of the studnt's name if it is in the name list.
+    del students_dict[remove_name]#  Allows the permanent deletion of the students record from the dictionary.
+    print("Student's details removed successfully!")# If the record is found it will remove the studnt's detail from all data structure.
 else:
-    print("Student not found!") #If not found, it will display "student not found".
+    print("Student's details not found!") #If the record or data not found it will print out not found.
